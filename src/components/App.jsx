f7884bd66655a52ac6a7cc1a388def2649be0e72
Hack Reactor Student <student@hackreactor.com>
2017-07-12 20:00:15 -0500

    Start work on the click functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,37 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData[0]} />
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData}/>
-    </div>
-  </div>
-);
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer video={window.exampleVideoData[0]} />
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList videos={window.exampleVideoData}/>
+//     </div>
+//   </div>
+// );
+
+class App {
+  constructor() {
+    this.state = {
+      current: window.exampleVideoData[0],
+      all: window.exampleVideoData
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer state={this.state} video={this.state.current} />
+        </div>
+        <div className="col-md-5">
+          <VideoList state={this.state} videos={this.state.all}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,5 +1,5 @@
-var VideoPlayer = ({video}) => (
-  <div className="video-player">
+var VideoPlayer = ({video, state}) => (
+  <div className="video-player" src={console.log(state)}>
     <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
     </div>
